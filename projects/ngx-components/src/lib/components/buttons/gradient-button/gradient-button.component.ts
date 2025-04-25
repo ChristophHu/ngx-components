f618e784011de595850b299da233e694dc24d234
christophhu <christoph.huschenhoefer@gmx.de>
2025-04-25 10:50:06 +0200

    add gradient-button

---
+++ projects/ngx-components/src/lib/components/buttons/gradient-button/gradient-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-gradient-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './gradient-button.component.html',
+  styleUrl: './gradient-button.component.css'
+})
+export class GradientButtonComponent {
+
+}
